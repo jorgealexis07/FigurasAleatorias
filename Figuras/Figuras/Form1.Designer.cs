392da266dcbe8c4f9c02d8dc081aaa588bed096e
jorgealexis07 <78820449+jorgealexis07@users.noreply.github.com>
2021-10-12 18:03:36 -0500

    Add files via upload

---
+++ Figuras/Figuras/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Figuras
+{
+    partial class FigurasAleatorias
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Tipo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar Figuras";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(302, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(342, 349);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Tipo
+            // 
+            this.Tipo.Location = new System.Drawing.Point(6, 33);
+            this.Tipo.Name = "Tipo";
+            this.Tipo.Size = new System.Drawing.Size(109, 20);
+            this.Tipo.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Tipo);
+            this.groupBox1.Location = new System.Drawing.Point(21, 251);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(121, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese el numero de lineas:";
+            // 
+            // FigurasAleatorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(641, 363);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FigurasAleatorias";
+            this.Text = "FigurasAleatorias";
+            this.TransparencyKey = System.Drawing.Color.LightPink;
+            this.Load += new System.EventHandler(this.FigurasAleatorias_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.TextBox Tipo;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Figuras/Figuras/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Figuras
+{
+    public partial class FigurasAleatorias : Form
+    {
+        public FigurasAleatorias()
+        {
+            InitializeComponent();
+            this.MouseClick += new MouseEventHandler(MiFuncionClick);
+        }
+        void MiFuncionClick(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            /*GeneradorNA();
+            label1.Text = GeneradorNA().ToString();*/
+            //COLORES
+            int A = r.Next(256);
+            int R = r.Next(256);
+            int G = r.Next(256);
+            int B = r.Next(256);
+            //COORDENADAS
+            int X = r.Next(500);
+            int Y = r.Next(500);
+            //longitud
+            int W = r.Next(500);
+            int H = r.Next(500);
+
+
+            //Creamos una variable tipo Bitmap
+            Bitmap b;
+
+            //Usamos el constructor para instanciar nuestro Bitmap y asignarle el tamaño
+            //de nuestro PictureBox (EN ESTE CASO EL PICTUREBOX TIENE COMO NOMBRE pictureBox1)
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            //Asignamos nuestro objeto Bitmap como imagen de nuestro pictureBox
+            pictureBox1.Image = (Image)b;
+
+            //Creamos el objeto Graphics que nos servirá para dibujar en nuestro Bitmap
+            Graphics g = Graphics.FromImage(b);
+
+            if (int.Parse(Tipo.Text) < 3)
+            {
+                MessageBox.Show("Numero no permitido!!! Debes ingresar numeros del 3 al 7");
+            }
+
+            else if (int.Parse(Tipo.Text) == 3)
+            {
+                // Create points that define polygon.
+                Point point1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point point2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point point3 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints =
+                    {
+                 point1,
+                 point2,
+                 point3,
+             };
+                System.Drawing.SolidBrush myBrush3 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush3, curvePoints);
+                System.Drawing.Graphics formGraphics3;
+                formGraphics3 = this.CreateGraphics();
+                formGraphics3.FillPolygon(myBrush3, curvePoints);
+                formGraphics3.Dispose();
+                myBrush3.Dispose();
+
+            }
+
+            else if (int.Parse(Tipo.Text) == 4)
+            {
+                // Create points that define polygon2.
+                Point poin1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin4 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints2 =
+                   {
+                 poin1,
+                 poin2,
+                 poin3,
+                 poin4,
+             };
+                System.Drawing.SolidBrush myBrush4 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush4, curvePoints2);
+                g.FillRectangle(myBrush4, new Rectangle((r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350))));
+                System.Drawing.Graphics formGraphics4;
+                formGraphics4 = this.CreateGraphics();
+                formGraphics4.FillPolygon(myBrush4, curvePoints2);
+                formGraphics4.FillRectangle(myBrush4, new Rectangle((r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350))));
+                myBrush4.Dispose();
+                formGraphics4.Dispose();
+
+            }
+            else if (int.Parse(Tipo.Text) == 5)
+            {
+                // Create points that define polygon3.
+                Point poi1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point[] curvePoints3 =
+                     {
+                 poi1,
+                 poi2,
+                 poi3,
+                 poi4,
+                 poi5,
+             };
+                System.Drawing.SolidBrush myBrush5 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush5, curvePoints3);
+                System.Drawing.Graphics formGraphics5;
+                formGraphics5 = this.CreateGraphics();
+                formGraphics5.FillPolygon(myBrush5, curvePoints3);
+                myBrush5.Dispose();
+                formGraphics5.Dispose();
+
+            }
+            else if (int.Parse(Tipo.Text) == 6)
+            {
+                // Create points that define polygon4.
+                Point po1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po6 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints4 =
+                     {
+                 po1,
+                 po2,
+                 po3,
+                 po4,
+                 po5,
+                 po6,
+             };
+                System.Drawing.SolidBrush myBrush6 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                System.Drawing.Graphics formGraphics6;
+                formGraphics6 = this.CreateGraphics();
+                g.FillPolygon(myBrush6, curvePoints4);
+                formGraphics6.FillPolygon(myBrush6, curvePoints4);
+                myBrush6.Dispose();
+                formGraphics6.Dispose();
+
+            }
+            else if (int.Parse(Tipo.Text) == 7)
+            {
+                // Create points that define polygon5.
+                Point p1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p6 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p7 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints5 =
+                     {
+                 p1,
+                 p2,
+                 p3,
+                 p4,
+                 p5,
+                 p6,
+                 p7,
+             };
+                System.Drawing.SolidBrush myBrush7 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush7, curvePoints5);
+                System.Drawing.Graphics formGraphics7;
+                formGraphics7 = this.CreateGraphics();
+                formGraphics7.FillPolygon(myBrush7, curvePoints5);
+                myBrush7.Dispose();
+                formGraphics7.Dispose();
+            }
+            else if (int.Parse(Tipo.Text) > 7)
+            {
+                MessageBox.Show("Numero no permitido!!! Solo debes ingresar numeros del 3 al 7");
+            }
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            /*GeneradorNA();
+            label1.Text = GeneradorNA().ToString();*/
+            //COLORES
+            int A = r.Next(256);
+            int R = r.Next(256);
+            int G = r.Next(256);
+            int B = r.Next(256);
+            //COORDENADAS
+            int X = r.Next(500);
+            int Y = r.Next(500);
+            //longitud
+            int W = r.Next(500);
+            int H = r.Next(500);
+
+
+            //Creamos una variable tipo Bitmap
+            Bitmap b;
+
+            //Usamos el constructor para instanciar nuestro Bitmap y asignarle el tamaño
+            //de nuestro PictureBox (EN ESTE CASO EL PICTUREBOX TIENE COMO NOMBRE pictureBox1)
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            //Asignamos nuestro objeto Bitmap como imagen de nuestro pictureBox
+            pictureBox1.Image = (Image)b;
+
+            //Creamos el objeto Graphics que nos servirá para dibujar en nuestro Bitmap
+            Graphics g = Graphics.FromImage(b);
+
+            if (int.Parse(Tipo.Text) < 3)
+            {
+                MessageBox.Show("Numero no permitido!!! Debes ingresar numeros del 3 al 7");
+            }
+
+            else if (int.Parse(Tipo.Text) == 3)
+            {
+                // Create points that define polygon.
+                Point point1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point point2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point point3 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints =
+                    {
+                 point1,
+                 point2,
+                 point3,
+             };
+                System.Drawing.SolidBrush myBrush3 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                    g.FillPolygon(myBrush3, curvePoints);
+                System.Drawing.Graphics formGraphics3;
+                formGraphics3 = this.CreateGraphics();
+                formGraphics3.FillPolygon(myBrush3, curvePoints);
+                formGraphics3.Dispose();
+                myBrush3.Dispose();
+                
+            }
+
+            else if (int.Parse(Tipo.Text) == 4)
+            {
+                // Create points that define polygon2.
+                Point poin1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poin4 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints2 =
+                   {
+                 poin1,
+                 poin2,
+                 poin3,
+                 poin4,
+             };
+                System.Drawing.SolidBrush myBrush4 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush4, curvePoints2);
+                g.FillRectangle(myBrush4, new Rectangle((r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350))));
+                System.Drawing.Graphics formGraphics4;
+                formGraphics4 = this.CreateGraphics();
+                formGraphics4.FillPolygon(myBrush4, curvePoints2);
+                formGraphics4.FillRectangle(myBrush4, new Rectangle((r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350)), (r.Next(0, 350))));
+                myBrush4.Dispose();
+                formGraphics4.Dispose();
+
+            }
+            else if (int.Parse(Tipo.Text) == 5)
+            {
+                // Create points that define polygon3.
+                Point poi1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point poi5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point[] curvePoints3 =
+                     {
+                 poi1,
+                 poi2,
+                 poi3,
+                 poi4,
+                 poi5,
+             };
+                System.Drawing.SolidBrush myBrush5 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush5, curvePoints3);
+                System.Drawing.Graphics formGraphics5;
+                formGraphics5 = this.CreateGraphics();
+                formGraphics5.FillPolygon(myBrush5, curvePoints3);
+                myBrush5.Dispose();
+                formGraphics5.Dispose();
+                
+            }
+            else if (int.Parse(Tipo.Text) == 6)
+            {
+                // Create points that define polygon4.
+                Point po1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point po6 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints4 =
+                     {
+                 po1,
+                 po2,
+                 po3,
+                 po4,
+                 po5,
+                 po6,
+             };
+                System.Drawing.SolidBrush myBrush6 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                System.Drawing.Graphics formGraphics6;
+                formGraphics6 = this.CreateGraphics();
+                g.FillPolygon(myBrush6, curvePoints4);
+                formGraphics6.FillPolygon(myBrush6, curvePoints4);
+                myBrush6.Dispose();
+                formGraphics6.Dispose();
+                
+            }
+            else if (int.Parse(Tipo.Text) == 7)
+            {
+                // Create points that define polygon5.
+                Point p1 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p2 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p3 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p4 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p5 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p6 = new Point(r.Next(0, 250), r.Next(0, 250));
+                Point p7 = new Point(r.Next(0, 250), r.Next(0, 250));
+
+                Point[] curvePoints5 =
+                     {
+                 p1,
+                 p2,
+                 p3,
+                 p4,
+                 p5,
+                 p6,
+                 p7,
+             };
+                System.Drawing.SolidBrush myBrush7 = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(A, R, G, B));
+                g.FillPolygon(myBrush7, curvePoints5);
+                System.Drawing.Graphics formGraphics7;
+                formGraphics7 = this.CreateGraphics();
+                formGraphics7.FillPolygon(myBrush7, curvePoints5);
+                myBrush7.Dispose();
+                formGraphics7.Dispose();
+            }
+            else if (int.Parse(Tipo.Text) > 7)
+            {
+                MessageBox.Show("Numero no permitido!!! Solo debes ingresar numeros del 3 al 7");
+            }
+        }
+
+        private void FigurasAleatorias_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
